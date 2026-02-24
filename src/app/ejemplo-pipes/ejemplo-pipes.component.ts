531af0620802d76189cc40a0b78287d2f8919982
AdrianGaray <adriangaray396@gmail.com>
2026-02-24 14:47:18 -0300

    Clase 50 - Agrega pipes para formateo de datos en Angular

---
+++ src/app/ejemplo-pipes/ejemplo-pipes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplo-pipes',
+  imports: [],
+  templateUrl: './ejemplo-pipes.component.html',
+  styleUrl: './ejemplo-pipes.component.css'
+})
+export class EjemploPipesComponent {
+
+}
