fdbce0401c3d4b59ff7f3a2d0cbc6db9626d3935
AdrianGaray <adriangaray396@gmail.com>
2026-02-25 14:59:39 -0300

    Capítulo 66 - Manejo de Rutas Hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ListadoUsuariosComponent } from './listado-usuarios/listado-usuarios.component';
 import { MostrarMensajeComponent } from './mostrar-mensaje/mostrar-mensaje.component';
+import { PadreComponent } from './padre/padre.component';
+import { HijoComponent } from './padre/hijo/hijo.component';
 
 
 // Routes → tipo de dato que define las rutas de la app
@@ -15,4 +17,10 @@ export const routes: Routes = [
     // Angular debe cargar: ListadoUsuariosComponent
     {path:'', component: ListadoUsuariosComponent}, //localhost:4200/
     {path:'mostrar-mensaje', component: MostrarMensajeComponent},
+    {
+        path:'configuracion', children: [
+            {path: 'padre', component: PadreComponent}, //localhost:4200/configuracion/padre
+            {path: 'hijo', component: HijoComponent} //localhost:4200/configuracion/hijo
+        ]
+    }
 ];
